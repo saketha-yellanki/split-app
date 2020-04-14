b85dec6cbb348d08a05f647ed1814c4e762f1e83
thaniav <61160291+thaniav@users.noreply.github.com>
2020-04-14 15:37:12 +0530

    Login Form UI
    Signed-off-by: thaniav <61160291+thaniav@users.noreply.github.com>

--- splitapp/app/src/main/java/com/splitapp/LoginActivity.java
+++ splitapp/app/src/main/java/com/splitapp/LoginActivity.java
@@ -1,7 +1,17 @@
 package com.splitapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannedString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +20,38 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        TextView SwitchToRegister = findViewById(R.id.switchToRegister);
+        String text = "New to SplitApp? Register here";
+        SpannableString ss = new SpannableString(text);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent myIntent = new Intent(LoginActivity.this, registration.class);
+                startActivity(myIntent);
+
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.WHITE);
+            }
+        };
+        ss.setSpan(clickableSpan, 16, 30, SpannedString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SwitchToRegister.setText(ss);
+        SwitchToRegister.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
+
+
+
+
+
+
     }
+
+
 }
--- splitapp/app/src/main/java/com/splitapp/registration.java
+++ splitapp/app/src/main/java/com/splitapp/registration.java
@@ -1,12 +1,20 @@
 package com.splitapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannedString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -21,6 +29,7 @@ public class registration extends AppCompatActivity {
     TextInputEditText mobile;
     Button register_btn;
     LinearLayout reg_lin_layout;
+    TextView SwitchToLogin;
 
     FirebaseAuth mAuth;
 
@@ -44,6 +53,29 @@ public class registration extends AppCompatActivity {
             }
         });
 
+
+        String text = "Already have an Account? Login.";
+        SpannableString ss = new SpannableString(text);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent myIntent = new Intent(registration.this, LoginActivity.class);
+                startActivity(myIntent);
+
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLACK);
+            }
+        };
+        ss.setSpan(clickableSpan, 0, 30, SpannedString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SwitchToLogin.setText(ss);
+        SwitchToLogin.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
     }
 
     @Override
@@ -65,5 +97,6 @@ public class registration extends AppCompatActivity {
         mobile = findViewById(R.id.mobile_edit);
         register_btn = findViewById(R.id.reg_btn);
         reg_lin_layout = findViewById(R.id.reg_layout);
+        SwitchToLogin = findViewById(R.id.switchToLogin);
     }
 }
