c140335943a41977d336a32153962edb85fee4d7
“SakethaYellanki“ <sakethayellanki05@gmail.com>
2020-04-18 13:05:16 +0530

    added splash screen.
    Signed-off-by: “SakethaYellanki“ <sakethayellanki05@gmail.com>

--- splitapp/app/src/main/java/com/splitapp/MainActivity.java
+++ splitapp/app/src/main/java/com/splitapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.splitapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, registration.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
     }
 }
