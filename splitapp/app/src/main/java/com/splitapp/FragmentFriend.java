f3fbaece04eb63a23898b8dda362e2332dc57502
harshithap99 <46523187+harshithap99@users.noreply.github.com>
2020-04-26 14:50:42 +0530

    fragments added

---
+++ splitapp/app/src/main/java/com/splitapp/FragmentFriend.java
@@ -0,0 +1,24 @@
+package com.splitapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentFriend extends Fragment {
+    View v;
+    public FragmentFriend() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        v = inflater.inflate(R.layout.friends,container,false);
+        return v;
+    }
+}
