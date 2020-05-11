8b79a8caedb772647e13f150cc1d229b4ea9e1de
“SakethaYellanki“ <sakethayellanki05@gmail.com>
2020-05-11 14:29:11 +0530

    1. Deleted extra expenses activities and res files. 2. Made changes to UI in add expenses activity.
    Signed-off-by: “SakethaYellanki“ <sakethayellanki05@gmail.com>

--- splitapp/app/src/main/java/com/splitapp/activities/AddExpenses.java
+++ splitapp/app/src/main/java/com/splitapp/activities/AddExpenses.java
@@ -1,16 +1,27 @@
 package com.splitapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.splitapp.R;
 
 public class AddExpenses extends AppCompatActivity {
+
+    TextInputEditText email_inp;
+    TextInputEditText amount_inp;
+    MaterialButton add_exp_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses);
+
+        email_inp = findViewById(R.id.email_et);
+        amount_inp = findViewById(R.id.amount_et);
+        add_exp_btn = findViewById(R.id.add_exp_btn);
+
     }
 }
