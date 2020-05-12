6c4e878bc9b862eb76f2eae42be37b8a21768944
Meghana862 <meghanreddy.avula@gmail.com>
2020-05-12 20:57:30 +0530

    Actionbar error resolved

--- splitapp/app/src/main/java/com/splitapp/activities/GroupMainActivity.java
+++ splitapp/app/src/main/java/com/splitapp/activities/GroupMainActivity.java
@@ -2,18 +2,27 @@ package com.splitapp.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.splitapp.R;
 
 public class GroupMainActivity extends AppCompatActivity {
@@ -43,10 +52,13 @@ public class GroupMainActivity extends AppCompatActivity {
         checkUser();
 
         AddFriendBtnGrp = findViewById(R.id.fab_btn_group_main);
+        final String user_id = firebaseAuth.getUid();
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
 
         AddFriendBtnGrp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent=new Intent(GroupMainActivity.this,GroupParticipantAddActivity.class);
                 intent.putExtra("groupTitle",groupTitle);
                 intent.putExtra("groupId",groupId);
@@ -54,6 +66,11 @@ public class GroupMainActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+
+
         /*toolbar=findViewById(R.id.toolbar1);
 
         setSupportActionBar(toolbar);
--- splitapp/app/src/main/java/com/splitapp/activities/GroupParticipantAddActivity.java
+++ splitapp/app/src/main/java/com/splitapp/activities/GroupParticipantAddActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -139,9 +140,13 @@ public class GroupParticipantAddActivity extends AppCompatActivity {
                                             actionBar.setTitle(g_title+"("+myGroupRole+")");
                                             getAllFriends();*/
                                                 for (final QueryDocumentSnapshot document1 : task.getResult()) {
-                                                    myGroupRole = document1.get("role").toString();
-                                                    actionBar.setSubtitle(groupTitle + "(" + myGroupRole + ")");
-                                                    getAllFriends();
+                                                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                                                    if(user.getUid().equals(document1.getId())){
+                                                        myGroupRole = document1.get("role").toString();
+                                                        actionBar.setSubtitle(groupTitle + "(" + myGroupRole + ")");
+                                                        getAllFriends();
+                                                    }
+
                                                 }
                                             } else {
 
